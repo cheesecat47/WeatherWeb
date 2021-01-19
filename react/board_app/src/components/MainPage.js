a2afc71d1ed4af5c2257a87d0e609be2176832c5
atg0831 <atg0831@naver.com>
2021-01-20 00:06:42 +0900

    feat: render boardList

--- react/board_app/src/components/MainPage.js
+++ react/board_app/src/components/MainPage.js
@@ -1,37 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import * as service from '../services/BoardApi';
+import { actionCreaters } from '../store';
 
-function MainPage({ id, user_id, name, article }) {
+//게시판 목록 조회
+
+function MainPage({ boardName, boardId}) {
+  
   return (
     <div className="card" >
       <div className="board">
-        <Link to={{
+        <h3>Name:{boardName}
+          {/* {renderBoard(boardList)} */}
+        </h3>
+        <h4>Id:{boardId}</h4>
+        
+        {/* {list[0].map(list => {
+          <h3>{list.name}</h3>
+        })} */}
+        {/* {boardList.map(board => board.name)} */}
+        {/* <Link to={{
           pathname: `/board/${id}`,
           state: {
-            id,
-            name,
-            user_id,
-            article
+            id: 
           }
+          
         }}>
           <h3>{name}</h3></Link>
         <div className="board_article">{ }
-          {article.map(article => (
             <Link to={{
               pathname: `/article/${article.article_id}`,
-              state: {
-                id,
-                name,
-                user_id,
-                article
-              }
-            }}> <p>{article.title}</p></Link>
-          ))}
-        </div>
+            }}> <p>{article.title}</p></Link> */}
+
       </div>
     </div>
   );
 }
 
-export default MainPage;
+// function mapStateToProps(state) {
+//   console.log(state);
+//   return { boardList: state };
+// }
+function mapDispatchToProps(dispatch) {
+  return {
+    createBoard: (data) => {
+      dispatch(actionCreaters.postContent(data))
+    }
+  };
+}
+export default connect(null, mapDispatchToProps)(MainPage);
