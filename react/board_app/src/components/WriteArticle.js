6366777fa192e2991ebd0c72691acd57f084e761
atg0831 <atg0831@naver.com>
2021-01-25 13:59:19 +0900

    Refactor: refactoring  write article form

--- react/board_app/src/components/WriteArticle.js
+++ react/board_app/src/components/WriteArticle.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState} from 'react';
 
 function WriteArticle() {
-    return(
-        <div>
-            this is write article page
-        </div>
-    )
+  const [title, setTitle] = useState("");
+  const [context, setContext] = useState("");
+  const detailEle = document.querySelector("details");
+  // console.log(detailEle);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    //여기에서 action 보내서 post 요청하는 logic 필요... 
+    
+    //글쓰기 창 접기...
+    if (title || context) {
+      detailEle.removeAttribute("open")
+    }
+    setContext("");
+    setTitle("");  
+  }
+
+  const onTitleChange = (e) => {
+    setTitle(e.target.value);
+  }
+
+  const onContextChange = (e) => {
+    setContext(e.target.value);
+  }
+
+  return (
+    <div>
+      <form className="write" onSubmit={onSubmit}>
+        <details >
+          <summary>새 글을 작성해주세요 </summary>
+          <p><input type="text" name="article_title" value={title} className="write_title" onChange={onTitleChange} placeholder="글 제목" ></input></p>
+          <textarea name="btn_write" value={context} cols="55" rows="20" onChange={onContextChange} placeholder="자유롭게 작성해주세요"></textarea>
+          <ul>
+            <button> 작성완료</button>
+          </ul>
+        </details>
+      </form>
+    </div>
+  )
 }
 
 export default WriteArticle;
--- react/board_app/src/routes/Board.js
+++ react/board_app/src/routes/Board.js
@@ -1,26 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import WriteArticle from '../components/WriteArticle';
 import '../css/Board.css';
 
 function Board({ location, history, match }) {
-  const [title, setTitle] = useState("");
-  const [ context, setContext ] = useState("");
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    // console.log(e.target);
-    //여기에서 action 보내서 post 요청하는 logic 필요... 
-    setContext("");
-    setTitle("");
-
-  }
-
-  const onTitleChange = (e) =>{
-    setTitle(e.target.value);
-  }
-  const onContextChange = (e) => {
-    setContext(e.target.value);
-  }
+ 
   useEffect(() => {
     if (location.state === undefined) {
       //match.params로 id값 받아와서 데이터 요청해야할듯...
@@ -38,16 +22,7 @@ function Board({ location, history, match }) {
         <div className="board_name">
           {boardName}
         </div>
-        <form className="write" onSubmit={onSubmit}>
-          <details >
-            <summary>새 글을 작성해주세요 </summary>
-            <p><input type="text" name="article_title" value={title} className="write_title" onChange={onTitleChange} placeholder="글 제목" ></input></p>
-            <textarea name="btn_write" value={context} cols="55" rows="20"onChange={onContextChange} placeholder="자유롭게 작성해주세요"></textarea>            <ul>
-            <button> 작성완료</button>
-            </ul>
-          </details>
-        </form>
-
+        <WriteArticle />
         <div className="articles">
           <div className="article">
             {/* article axios.get으로 데이터 받아오기... */}
